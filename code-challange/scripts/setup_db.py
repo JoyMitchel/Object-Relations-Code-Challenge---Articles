38e240baae7cf116d835aef5be69fc65a064aee7
joy.mitchel@student.moringaschool.com <JoyMitchel>
2025-05-27 11:35:25 +0300

    seed db

--- code-challange/scripts/setup_db.py
+++ code-challange/scripts/setup_db.py
@@ -1,2 +1,10 @@
 import sqlite3
 from lib.db.connection import get_connection
+
+with open('code-challenge/lib/db/schema.sql', 'r') as f:
+    schema_sql = f.read()
+
+conn = get_connection()
+conn.executescript(schema_sql)
+conn.commit()
+conn.close()
