2170a9ae74a862980c621dabff38b0a7e94bd7d2
joy.mitchel@student.moringaschool.com <JoyMitchel>
2025-05-27 11:32:46 +0300

    debugged

--- code-challange/scripts/run_queries.py
+++ code-challange/scripts/run_queries.py
@@ -3,26 +3,30 @@ from lib.models.magazine import Magazine
 from lib.models.article import Article
 
 if __name__ == "__main__":
-
-     alice = Author.find_by_name("Alice")
+   
+    alice = Author.find_by_name("Alice")
     if alice:
         print(f"Articles by {alice.name}:")
         for article in alice.articles:
             print(f"- {article.title}")
 
-       if alice:
+  
+    if alice:
         print(f"Magazines {alice.name} contributed to:")
         for mag in alice.magazines:
             print(f"- {mag.name} ({mag.category})")
-       
-        tech_today = Magazine.find_by_name("Tech Today")
+
+   
+    tech_today = Magazine.find_by_name("Tech Today")
     if tech_today:
         print(f"Contributors to {tech_today.name}:")
         for author in tech_today.contributors:
             print(f"- {author.name}")
 
-
+   
     if tech_today:
         print(f"Articles in {tech_today.name}:")
-        for article in tech_today.articles:
-            print(f"- {article.title} by {article.author.name}")
+        for title in tech_today.article_titles:
+            print(f"- {title}")
+            
+
